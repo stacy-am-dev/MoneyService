434074c4a400fe315188ee341aae239a991a5456
OverSadBoy <paramime151@gmail.com>
2020-11-07 08:58:59 +0400

    Базовая настройка проекта: Организация файлов сборки, модулей, gitignore

--- app/src/main/java/git/stacyamdev/moneyservice/MainActivity.kt
+++ app/src/main/java/git/stacyamdev/moneyservice/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.moneyservice
+package git.stacyamdev.moneyservice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.moneyservice.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
